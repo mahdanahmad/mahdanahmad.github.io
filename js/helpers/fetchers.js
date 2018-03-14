18f89fccdebe4f6f402d857a92f0656a34a52900
Mahdan Ahmad Fauzi Al-Hasan <mahdan.ahmad@yahoo.com>
2018-03-14 16:14:06 +0700

    task 1

--- js/helpers/fetchers.js
+++ js/helpers/fetchers.js
@@ -1,9 +1,9 @@
 const baseURL	= 'http://139.59.230.55/frontend/api/maps/';
 
-function getDisaster(callback) { $.getJSON( baseURL + 'disaster', (data) => { callback(data); } ); }
-function getVulnerable(callback) { $.getJSON( baseURL + 'vulnerable', (data) => { callback(data); } ); }
-function getArea(callback) { $.getJSON( baseURL + 'area', (data) => { callback(data); } ); }
+function getDisaster(callback) { $.getJSON( baseURL + 'disaster', (data) => { callback(null, data); } ); }
+function getVulnerable(callback) { $.getJSON( baseURL + 'vulnerable', (data) => { callback(null, data); } ); }
+function getArea(callback) { $.getJSON( baseURL + 'area', (data) => { callback(null, data); } ); }
 
 function getOdpair(callback) { $.getJSON( baseURL.replace('maps', 'odpair'), (data) => { callback(data); } ); }
 
-function getTopojson(callback) { $.getJSON( 'https://raw.githubusercontent.com/pemiluAPI/pemilu-data/master/dapil/shapefiles/admin_kabupaten/geojson/KABUKOTA_ADMINISTRATIVE_AREA-SIMPLIFIED.topojson', (data) => { callback(data); } ); }
+function getTopojson(callback) { $.getJSON( 'https://raw.githubusercontent.com/pemiluAPI/pemilu-data/master/dapil/shapefiles/admin_kabupaten/geojson/KABUKOTA_ADMINISTRATIVE_AREA-SIMPLIFIED.topojson', (data) => { callback(null, data); } ); }
