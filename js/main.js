1641bef51a767e4fa07f012641e5d61046541484
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2019-02-06 15:48:22 +0700

    init

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
 $( document ).ready(function() {
 
-	initTask1();
-	initTask2();
-
-	$( 'i.fa-chevron-down' ).click((o) => {
-		$( 'i.fas' ).hide();
-
-		$( '#task1-wrapper' ).slideUp(() => { $( 'i.fas' ).show(); });
-		$( '#task2-wrapper' ).show();
-	});
-	$( 'i.fa-chevron-up' ).click((o) => {
-		$( 'i.fas' ).hide();
-
-		$( '#task2-wrapper' ).hide();
-		$( '#task1-wrapper' ).slideDown(() => { $( 'i.fas' ).show(); });
-	});
 });
